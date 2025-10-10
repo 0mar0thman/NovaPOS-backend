cf3686e3551de6e3f4bcd331f13ab5158199b207
Omar Mohamed <omar.mohamed11221@gmail.com>
2025-10-10 03:01:05 +0300

    v11.1

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,48 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Response;
 use Laravel\Sanctum\Http\Controllers\CsrfCookieController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| - /home => يعرض Laravel (مثلاً welcome.blade.php)
+| - / => يعرض React (index.html)
+| - /api/... => يظل للـ API
+| - أي Route تاني => fallback إلى React
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
+})->name('home');
+
+// Route لتأمين CSRF لـ Sanctum (لو بتستخدم React + Laravel API)
+Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show'])
+    ->name('sanctum.csrf');
+
+// المسار الأساسي: يفتح تطبيق React
+Route::get('/', function () {
+    $path = public_path('index.html');
+
+    if (File::exists($path)) {
+        return Response::file($path);
+    }
+
+    abort(404, 'React app not found.');
 });
 
+// fallback لأي Route غير معروف (علشان React Router)
+Route::fallback(function () {
+    $path = public_path('index.html');
 
-// Route::get('/sanctum/csrf-cookie', [CsrfCookieController::class, 'show']);
+    if (File::exists($path)) {
+        return Response::file($path);
+    }
 
-Route::get('/sanctum/csrf-cookie', function () {
-    return response()->noContent();
+    abort(404, 'Page not found.');
 });
