e0d715c6f42423bb84cbb46448fafc40b9519a24
Omar Mohamed <omar.mohamed11221@gmail.com>
2025-10-10 19:02:07 +0300

    v11.5

--- config/cors.php
+++ config/cors.php
@@ -17,8 +17,8 @@ return [
 
     'allowed_origins' => [
         'https://pos-nova.vercel.app',
-        'http://localhost:3000',
-        'http://127.0.0.1:3000',
+        'http://localhost:8080',
+        'http://127.0.0.1:8080',
     ],
 
     'allowed_origins_patterns' => [],
